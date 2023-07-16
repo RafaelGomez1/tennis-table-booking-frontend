f4fd65c847bfd12a0e87db6ebe0afb0ac6bbdda8
rgomez <rafael.gomez@code.seat>
2023-07-16 17:11:01 +0200

    ADD angular project and docker and deployment to do

--- tennis-table-booking-reservation/src/app/app.module.ts
+++ tennis-table-booking-reservation/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LogoComponent } from './logo/logo.component';
+import { ListViewComponent } from './list-view/list-view.component';
+import { IniciarSesionComponent } from './iniciar-sesion/iniciar-sesion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogoComponent,
+    ListViewComponent,
+    IniciarSesionComponent
   ],
   imports: [
     BrowserModule,
