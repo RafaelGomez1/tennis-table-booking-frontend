b4749424490092bd3055afa3eb2721f8a219477f
rgomez <rafael.gomez@code.seat>
2023-07-27 10:28:35 +0200

    ADD UX improvements

--- tennis-table-booking-reservation/src/app/components/action-dialog/action-dialog.component.ts
+++ tennis-table-booking-reservation/src/app/components/action-dialog/action-dialog.component.ts
@@ -2,6 +2,7 @@ import {Component, Inject} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
 import {Agenda, AvailableHour} from "../../models/agenda/Agenda";
 import {AgendaService} from "../../services/agenda.service";
+import {Action} from "../../models/action/Action";
 
 @Component({
   selector: 'app-action-dialog',
@@ -21,15 +22,16 @@ export class ActionDialogComponent {
     @Inject(MAT_DIALOG_DATA) private data: any
   ) {
     this.agenda = data.agenda;
-    this.selectedHour = data.selectedHour
-    this.agendaDateFormatted = data.agendaDateFormatted
+    this.selectedHour = data.selectedHour;
+    this.reservationType = this.decideDefaultReservationValue()
+    this.agendaDateFormatted = data.agendaDateFormatted;
   }
 
   onSubmit(): void {
-    if(this.reservationType == 'RESERVAR') {
+    if(this.reservationType == 'Reservar') {
       this.agendaService.bookSlot(this.agenda.id, this.selectedHour.id, this.name)
         .subscribe(agenda => this.dialogRef.close(agenda))
-    } else if (this.reservationType == 'CANCELAR_RESERVA') {
+    } else if (this.reservationType == 'Cancelar Reserva') {
       this.agendaService.cancelBooking(this.agenda.id, this.selectedHour.id, this.name)
         .subscribe(agenda => this.dialogRef.close(agenda))
     } else { this.dialogRef.close(this.agenda) }
@@ -37,5 +39,21 @@ export class ActionDialogComponent {
 
   onCancel() {
     this.dialogRef.close();
+  }
+
+  protected readonly parseInt = parseInt;
+
+  reservationTypeOptions(): string[] {
+    if (this.selectedHour.registeredPlayers.length === parseInt(this.selectedHour.capacity.value, 10)) {
+      return ['Cancelar Reserva']
+    } else if (this.selectedHour.registeredPlayers.length === 0) {
+      return ['Reservar']
+    } else return ['Reservar', 'Cancelar Reserva']
+  }
+
+  decideDefaultReservationValue() {
+    if (this.selectedHour.registeredPlayers.length === parseInt(this.selectedHour.capacity.value, 10)) {
+      return 'Cancelar Reserva'
+    } else return 'Reservar'
   }
 }
