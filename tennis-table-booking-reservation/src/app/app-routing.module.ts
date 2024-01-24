bb294f28627185c58a04fff315eaf8a875e3a011
rgomez <rafael.gomez@code.seat>
2024-01-24 22:42:01 +0100

    WIP Add League Ranking

--- tennis-table-booking-reservation/src/app/app-routing.module.ts
+++ tennis-table-booking-reservation/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: 'ranking', component: RankingPageComponent },
+  // Add other routes if needed
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ tennis-table-booking-reservation/src/app/models/ranking/LeagueRanking.ts
@@ -0,0 +1,35 @@
+export class PlayerStats {
+  constructor(
+    public gamesPlayed: number,
+    public gamesWon: number,
+    public gamesLost: number,
+    public winRate: number
+  ) {}
+}
+
+export class Player {
+  constructor(
+    public id: number,
+    public name: string,
+    public club: string,
+    public stats: PlayerStats,
+    public rankingPoints: number
+  ) {}
+}
+
+export class RankingData {
+  constructor(
+    public id: string,
+    public name: League,
+    public players: Player[]
+  ) {}
+}
+
+export enum League {
+  PREFERENT ="PREFERENT",
+  PRIMERA ="PRIMERA",
+  SEGUNDA_A ="SEGUNDA_A",
+  SEGUNDA_B ="SEGUNDA_B",
+  TERCERA_A ="TERCERA_A",
+  TERCERA_B ="TERCERA_B",
+}
